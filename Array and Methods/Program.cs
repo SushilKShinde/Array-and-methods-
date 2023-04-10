f654c60bcdb4a9c078776ee02cc67b8d143cc89e
Sushil <skshinde2028@gmail.com>
2023-04-10 18:21:06 +0530

    [Sushil] adding method

--- Array and Methods/Program.cs
+++ Array and Methods/Program.cs
@@ -4,34 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
 namespace Array_and_Methods
 {
     internal class Program
     {
+        static void MyMethod()
+        {
+            Console.WriteLine("This is my first method");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-
-            //2D array 
-            int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
-            //foreach loop
-            foreach(int number in numbers)
-            {
-                Console.WriteLine(number);
-            }
-            Console.ReadLine();
-
-            //for loop
-            for (int i=0; i<numbers.GetLength(0); i++)
-            {
-                for(int j=0; j<numbers.GetLength(1); j++)
-                {
-                    Console.WriteLine(numbers[i, j]);
-                }
-            }
-            Console.ReadLine();
+            // method is a block of code which can be reused and it can be called by its name
+            // methods are also called as Functions 
+           
+            MyMethod();
         }
     }
 }
