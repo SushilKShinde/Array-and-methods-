e805574e70dcab87c69c69465eb70962968cd077
Sushil <skshinde2028@gmail.com>
2023-04-10 18:52:14 +0530

    [Sushil] code for method and its types

--- Array and Methods/Program.cs
+++ Array and Methods/Program.cs
@@ -8,9 +8,25 @@ namespace Array_and_Methods
 {
     internal class Program
     {
-        static void MyMethod()
+        //method with single parameter
+        static void MyMethod1(int x)
         {
-            Console.WriteLine("This is my first method");
+            Console.WriteLine(x+5);
+        }
+        //method with multiple parameter
+        static void MyMethod2(int x, int y)
+        {
+            Console.WriteLine(x+y);
+        }
+        //method returning result
+        static int MyMethod3(int x, int y)
+        {
+            return x + y;
+        }
+        //method with default parameters
+        static void MyMethod4(string country="India")
+        {
+            Console.WriteLine(country);
         }
         static void Main(string[] args)
         {
@@ -18,7 +34,12 @@ namespace Array_and_Methods
             // method is a block of code which can be reused and it can be called by its name
             // methods are also called as Functions 
            
-            MyMethod();
+            MyMethod1(8);
+            MyMethod2(5, 4);
+            int result = MyMethod3(5, 6);
+            Console.WriteLine(result);
+            MyMethod4("Japan");
+            MyMethod4();
         }
     }
 }
