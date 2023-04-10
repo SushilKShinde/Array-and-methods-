b769a24ec7c40fec20808c2e967ffad50515b2f2
Sushil <skshinde2028@gmail.com>
2023-04-10 18:08:14 +0530

    [Sushil] adding array and methods folder

---
+++ Array and Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Array_and_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            //2D array 
+            int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+            //foreach loop
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+
+            //for loop
+            for (int i=0; i<numbers.GetLength(0); i++)
+            {
+                for(int j=0; j<numbers.GetLength(1); j++)
+                {
+                    Console.WriteLine(numbers[i, j]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
